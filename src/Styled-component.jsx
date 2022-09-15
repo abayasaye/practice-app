e9811a9f9776bdc6596d60b7083cd829bac97d06
abayasaye <abay400@gmail.com>
2022-09-15 10:20:07 +0300

    first commit

---
+++ src/Styled-component.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+const StyledComponent = ()=>{
+const Button = styled.button`
+background-color:#fff;
+color:#000;
+`
+return(
+    <>
+    <Button>CLICK</Button>
+    </>
+)
+}
+export default StyledComponent
